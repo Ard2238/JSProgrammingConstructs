50c4735ab1db85ed3323136b95198c67061df9d1
Ard2238 <abhishekrdas28@gmail.com>
2020-11-09 18:34:19 +0530

    Refactored Code to convert between different units

--- UnitConversionCase.js
+++ UnitConversionCase.js
@@ -1,29 +1,26 @@
 const prompt = require('prompt-sync')();
 
-let choice = prompt("1. Feet to Inch \n2. Inch To Feet \n3. Feet To Meter \n4. Meter To Feet");
+console.log("1. Feet to Inch \n2. Inch To Feet \n3. Feet To Meter \n4. Meter To Feet");
+let choice = prompt("Enter a choice: ");
+choice = parseInt(choice);
 switch(choice){
-    case 1:{
-        let num = prompt("Enter the number in Feet : ");
-        console.log(num * 12);
+    case 1:
+        let num1 = prompt("Enter the number in Feet : ");
+        console.log(num1 * 12);
         break;
-    }
-    case 2:{
-        let num = prompt("Enter the number in Meter : ");
-        console.log(num / 12);
+    case 2:
+        let num2 = prompt("Enter the number in Inch : ");
+        console.log(num2 / 12);
         break;
-    }
-    case 3:{
-        let num = prompt("Enter the number in Feet : ");
-        console.log(num * 3 / 10);
+    case 3:
+        let num3 = prompt("Enter the number in Feet : ");
+        console.log(num3 * 3 / 10);
         break;
-    }
-    case 4:{
-        let num = prompt("Enter the number in Meter : ");
-        console.log(num * 10 / 3);
+    case 4:
+        let num4 = prompt("Enter the number in Meter : ");
+        console.log(num4 * 10 / 3);
         break;
-    }
-    default:{
+    default:
         console.log("Wrong Input !");
-    }
 }
 
