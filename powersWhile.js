4f91723e9748b359268a49e3aeb421921530f597
Ard2238 <abhishekrdas28@gmail.com>
2020-11-09 11:27:03 +0530

    Added Code to calculate powers of 2

--- powersWhile.js
+++ powersWhile.js
@@ -1,10 +1,10 @@
 const prompt = require('prompt-sync')();
 
 let input = prompt("Enter a number : ");
-let num = 1;
+let count = 0;
 let i = 1;
-while(num != 256 || i <= input){
-    num = num * i
-    console.log(num);
+while(Math.pow(2,i) <= 256 && count <= input){
+    console.log(Math.pow(2,i));
     i ++;
+    count ++;
 }
